9d2957bdb4e303213e0380b1ca0b51a394f696cc
ddddddO <lbfdeatq@gmail.com>
2019-03-21 22:34:53 +0900

    05

---
+++ capter1/ngram.py
@@ -0,0 +1,28 @@
+#https://kotobank.jp/word/N%E3%82%B0%E3%83%A9%E3%83%A0-1702302
+
+def bi_gram_word(target):
+    words = target.replace(',', '').rstrip('.').split(' ')
+    bi_gram_list = []
+
+    for i in range(len(words)-1):
+        bi_gram = words[i] + words[i+1]
+        bi_gram_list.append(bi_gram)
+
+    return bi_gram_list
+
+def bi_gram_char(target):
+    text = target.replace(' ', '')
+    bi_gram_list = []
+
+    for i in range(len(text)-1):
+         bi_gram = text[i:i+2]
+         bi_gram_list.append(bi_gram)
+
+    return bi_gram_list
+
+
+target = "I am an NLPer"
+
+print('src: {}'.format(target))
+print(bi_gram_word(target))
+print(bi_gram_char(target))
