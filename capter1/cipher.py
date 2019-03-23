1fb7ed53f045aa05b66c4c44d1d28e93da3ab70b
ddddddO <lbfdeatq@gmail.com>
2019-03-23 14:03:29 +0900

    encrypt function

--- capter1/cipher.py
+++ capter1/cipher.py
@@ -1,27 +1,42 @@
+# coding:utf-8
 import argparse
 
 def decrypt(target):
-    print(target)
     print('DECRIPT')
-
-def encrypt(target):
     print(target)
-    print('ENCRYPT')
+    
 
-parser = argparse.ArgumentParser(description='encryption and decryption')
-subparsers = parser.add_subparsers()
+# https://www.k-cube.co.jp/wakaba/server/ascii_code.html
+def encrypt(target):
+    print('ECRIPT')
+    print(target)
 
-# encrypt
-parser_encrypt = subparsers.add_parser('encrypt')
-parser_encrypt.add_argument('-t', '--target', type=str, help='encrypt!')
-parser_encrypt.set_defaults(handler=encrypt)
+    encrypted_text = ''
+    for char in target:
+        n = ord(char)
+        if 97 <= n and n <= 122:
+            encrypted_text += chr(219-n)
+        else:
+            encrypted_text += char
+    
+    print(encrypted_text)
 
-# decrypt
-parser_decrypt = subparsers.add_parser('decrypt')
-parser_decrypt.add_argument('-t', '--target', type=str, help='decrypt!')
-parser_decrypt.set_defaults(handler=decrypt)
+# cipher.py encrypt/decrypt -t <str>
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='encryption and decryption')
+    subparsers = parser.add_subparsers()
 
-args = parser.parse_args()
+    # encrypt
+    parser_encrypt = subparsers.add_parser('encrypt')
+    parser_encrypt.add_argument('-t', '--target', type=str, help='encrypt!')
+    parser_encrypt.set_defaults(handler=encrypt)
 
-if hasattr(args, 'handler'):
-    args.handler(args.target)
+    # decrypt
+    parser_decrypt = subparsers.add_parser('decrypt')
+    parser_decrypt.add_argument('-t', '--target', type=str, help='decrypt!')
+    parser_decrypt.set_defaults(handler=decrypt)    
+
+    args = parser.parse_args()
+
+    if hasattr(args, 'handler'):
+        args.handler(args.target)
